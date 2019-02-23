9cd3be3e12424cea423c6471887bbedc968f46b6
mac <mac@macdeMacBook-Pro.local>
2019-02-23 15:31:37 -0500

    ok

---
+++ precache-manifest.71f401f5e6c54250d0a8759cc19822d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e12d85154bd31adb64e9",
+    "url": "/static/js/main.e12d8515.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e12d85154bd31adb64e9",
+    "url": "/static/css/main.08b920be.chunk.css"
+  },
+  {
+    "revision": "4a663774a057b9f7537a0d9841ccc576",
+    "url": "/index.html"
+  }
+];
